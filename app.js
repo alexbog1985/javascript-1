463169a860203419089d30b82b6d6e174272e802
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-01-30 00:20:09 +0300

    deposit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const deposit = 12000;
+const rate = 0.07;
+const depositLength = 24;
+const houseCost = 13500;
+
+const res = deposit * (1 + rate / 12) ** 24;
+if (res > houseCost) {
+    console.log(`Может купить. Остаток ${res - houseCost}`);
+} else {
+    console.log('Купить не сможем((');
+}
